25737041a9b0621b500884afb7302d2a9195fac0
spasea <ithespv@gmail.com>
2017-08-22 22:32:34 +0300

    install clean boilerplate

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,6 @@
 export default [
   {
     path: '/about/',
-    component: require('./components/pages/about.vue')
-  },
-  {
-    path: '/form/',
-    component: require('./components/pages/form.vue')
-  },
-  {
-    path: '/dynamic-route/blog/:blogId/post/:postId/',
-    component: require('./components/pages/dynamic-route.vue')
+    component: require('./components/pages/About.vue')
   }
 ]
